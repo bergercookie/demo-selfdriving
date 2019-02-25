bb9da4df039e25ea2d0634a354aab3fdeb4a043d
Nikos Koukis <nikolaos@slamcore.com>
2019-02-25 20:36:19 +0000

    Do basic I/O

--- basic_types.py
+++ basic_types.py
@@ -1,9 +1,45 @@
 # Contains the basic types for the HashCode Problem
+from utils import print_
+
+class Point(object):
+    def __init__(self, r=-1, c=-1):
+        self.r = r
+        self.c = c
+
+    def __repr__(self):
+        msg = "({},{})".format(self.r, self.c)
+        return msg
+
+
+class Ride(object):
+    def __init__(self, start, end, t_start, t_end):
+        self.start = start
+        self.end = end
+        self.t_start = t_start
+        self.t_end = t_end
+
+    def __repr__(self):
+        msg =  "* start: {}\n".format(self.start)
+        msg += "  * end: {}\n".format(self.end)
+        msg += "  * t_start: {}\n".format(self.t_start)
+        msg += "  * t_end: {}\n\n".format(self.t_end)
+
+        return msg
 
 class Problem:
     """Container for the whole problem."""
 
     def __init__(self):
         pass
+
+        # Totals
+        self.R = -1
+        self.C = -1
+        self.F = -1
+        self.N = -1
+        self.B = -1
+        self.T = -1
+
+        self.rides = []
 
     # TODO - Add viewers to the data
--- parse_input.py
+++ parse_input.py
@@ -1,4 +1,4 @@
-from basic_types import Problem
+from basic_types import Problem, Ride, Point
 from utils import print_, print_sec, print_ssec, sep, ssep
 
 
@@ -7,7 +7,6 @@ def parse_input(f_in: str):
     print_ssec("parsing input...")
 
     # Read all contents - line by line
-    # TODO - Change me if not dealing with ints!!!
     with open(f_in, 'r') as f:
         conts = [[int(s) for s in i.rstrip().split()] for i in f.readlines()]
 
@@ -15,13 +14,34 @@ def parse_input(f_in: str):
     if conts[-1] == '':
         conts = conts[:-1]
 
-    # Total values
+    prob = Problem()
 
-    # Get rest of items
+    # Total values
+    prob.R, prob.C, prob.F, prob.N, prob.B, prob.T = conts[0]
+
+    # Get rest of items - N Rides
+    line = 1
+    for line in range(1, prob.N + 1):
+        c = conts[line]
+        line += 1
+
+        start = Point(c[0], c[1])
+        end = Point(c[2], c[3])
+        t_start, t_end = c[4], c[5]
+        prob.rides.append(Ride(start, end, t_start, t_end))
 
     # Print stuff out
     print_ssec("Inputs: ")
+    print("R: ", prob.R)
+    print("C: ", prob.C)
+    print("F: ", prob.F)
+    print("N: ", prob.N)
+    print("B: ", prob.B)
+    print("T: ", prob.T)
+    # print_("R: {}| C: {}| F: {}| N: {}| B: {}| T: {}".format(
+    #     prob.R, prob.C, prob.F, prob.N, prob.B, prob.T
+    # ))
 
+    print("rides:\n", prob.rides)
     print_("parsed input successfully")
-    return Problem()
 
